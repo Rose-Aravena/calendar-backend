ffb22dcabdc33bc0a2f4cae545c57b838b716dd7
Rose Aravena Madariaga <rosearavenamadariaga@MacBook-Air-de-Rose.local>
2022-10-24 11:44:07 -0300

    settings for user creation

---
+++ database/config.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const dbConnection = async() => {
+    try {
+        await mongoose.connect(process.env.DB_CNN);
+        console.log('DB Online')
+
+    } catch (error) {
+        console.log(error);
+        throw new Error('Error a la hora de inicializar BD');
+    }
+}
+
+module.exports= {
+    dbConnection
+}
